4899056d407167cce99c44c9d9a6d58cc5eae4dc
Eduardo Martinez <eduardo.martinez@hrmedia.center>
2022-09-21 14:22:25 -0500

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,22 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "mi primer tarea",
+    description: "es la primer tarea",
+  },
+  {
+    id: 1,
+    title: "mi segunda tarea",
+    description: "es la segunda tarea",
+  },
+  {
+    id: 2,
+    title: "mi tercer tarea",
+    description: "es la tercer tarea",
+  },
+  {
+    id: 3,
+    title: "mi cuarta tarea",
+    description: "es la cuarta tarea",
+  },
+];
